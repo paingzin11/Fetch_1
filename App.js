88dd2a16b060a660052fe2ba9e6c10c4057f987a
paingzin11 <paingzinphyo040@gmail.com>
2023-01-06 12:36:54 +0630

    bbb

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {  useState, useEffect } from 'react';
+import { Text,StyleSheet, FlatList, View } from 'react-native';
+import { Card, Paragraph } from 'react-native-paper';
 
-export default function App() {
+
+export default function App () {
+  const API =  'https://reactnative.dev/movies.json';
+  const [Data, setData] = useState([]);
+
+  const fetchPost = async() => {
+    try{
+      const response = await fetch(API);
+      const json = await response.json();
+      setData(json.movies);
+    }
+    catch(error){
+      console.error(error);
+    }
+  };
+
+  useEffect(()=> {
+    fetchPost()
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+        <FlatList
+          data={Data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Card style={styles.cardbox}>
+              <Text style={styles.title}>Movie ID : {item.id}</Text>
+              <Text >{item.title}</Text>
+              <Paragraph>{item.releaseYear}</Paragraph>
+            </Card>
+          )}
+        />
+      
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    textAlign: 'center',
     justifyContent: 'center',
+    marginHorizontal: 10,
+    marginVertical: 15,
   },
+  cardbox: {
+    marginVertical:8,
+    marginHorizontal:5,
+    padding: 10,
+    backgroundColor: '#FF731D'
+  }, 
+  title: {
+    backgroundColor: '#5F9DF7',
+    fontWeight: 'bold', 
+    textAlign:  'left',
+    paddingLeft: 10,
+    borderRadius: 5,
+    paddingVertical: 5 ,
+  }
+
 });
